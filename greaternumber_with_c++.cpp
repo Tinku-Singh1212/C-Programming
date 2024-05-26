7692e7bf078d105b6d907665ffda6a53cf57cff5
TINKU SINGH <154521515+Tinku-Singh1212@users.noreply.github.com>
2024-05-26 23:46:23 +0530

    26/05/24

---
+++ greaternumber_with_c++.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+using namespace std; 
+#include<stdlib.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c;
+	cout<<"hello tinku singh"<<endl;
+	cout<<"enter the number a, b, and c";
+	cin>>a>>b>>c;
+	if(a>b&&a>>c)
+	{
+		cout<<"number a is greater="<<a;
+	}
+	else if(b>c&&b>a)
+	{
+		cout<<"number b is greater="<<b;
+	}
+	else
+	{
+		cout<<"number c is greater="<<c;
+	}
+	
+	return 0;
+}
