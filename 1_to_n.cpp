496073f75fc876fe607420ee66944fa1c56737cf
TINKU SINGH <154521515+Tinku-Singh1212@users.noreply.github.com>
2024-08-01 22:22:47 +0530

    1/08/24

---
+++ 1_to_n.cpp
@@ -0,0 +1,14 @@
+// Print 1 to n number
+#include<stdio.h>
+int main()
+{
+	int n,i=1;
+	printf("enter the limit :");
+	scanf("%d",&n);
+	while(i<=n)
+	{
+		printf("%d\n",i);
+		i++;
+	}
+	return 0;
+}
