899c13a09818a29ad1f10a8f07f9b9fbc394770d
TINKU SINGH <154521515+Tinku-Singh1212@users.noreply.github.com>
2024-08-10 21:52:22 +0530

    10/08/24

---
+++ perameter_rectangle.cpp
@@ -0,0 +1,13 @@
+//WAP to calculate the area and perimeter of rectangle.
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int length=10,breathe=7;
+int area,perimeter;
+area=length*breathe;
+printf("\n The area of rectangle = %d",area);
+perimeter=2*(length+breathe);
+printf("\n The perimeter of rectangle= %d",perimeter);
+return 0;
+}
