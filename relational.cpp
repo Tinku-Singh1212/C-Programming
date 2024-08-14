52c9af6c277cdb7903453e54899131e45e368eb5
TINKU SINGH <154521515+Tinku-Singh1212@users.noreply.github.com>
2024-08-14 22:14:59 +0530

    14/08/24

---
+++ relational.cpp
@@ -0,0 +1,14 @@
+//WAP to understand the concept of relational operators(<,<=,>,>=,==,!=)
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a=4,b=6;
+printf("%d",(4<6));
+printf("\n%d",(5<=6));
+printf("\n%d",(20>12));
+printf("\n%d",(10>=20));
+printf("\n%d",(10==10));
+printf("\n%d",(90!=90));
+return 0;
+}
